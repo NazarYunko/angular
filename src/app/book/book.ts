b41c74a9e0e28c55155bb6796553af402a13d53b
Nazar Yunko <yunko.nazar@gmail.com>
2017-10-15 16:53:45 +0300

    book update

--- src/app/book/book.ts
+++ src/app/book/book.ts
@@ -1,15 +1,25 @@
 export class Book {
 
+  private _id: number;
   private _name: string;
   private _author: string;
   private _price: string;
   private _image: string;
 
-  constructor(name: string, author: string, price: string, image: string) {
+  constructor(id: number, name: string, author: string, price: string, image: string) {
+    this._id = id;
     this._name = name;
     this._author = author;
     this._price = price;
     this._image = image;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
   }
 
   get name(): string {
